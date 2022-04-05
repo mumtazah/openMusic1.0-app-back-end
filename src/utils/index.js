75eb1d00269a60d6f095c117661046cdf7b731c5
Mumtazah Azma <74398823+mumtazah@users.noreply.github.com>
2022-04-05 11:37:52 +0700

    Add files via upload

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable camelcase */
+const mapDBToModel = ({
+  id, title, year, performer, genre, duration, album_id,
+}) => ({
+  id,
+  title,
+  year,
+  performer,
+  genre,
+  duration,
+  albumId: album_id,
+});
+
+module.exports = { mapDBToModel };
